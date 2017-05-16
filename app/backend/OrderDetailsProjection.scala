a5f2db8e44775a205461a008cad78e1fe6a4b9d7
Luc Duponcheel <luc@strongtyped.io>
2017-05-16 13:31:56 +0200

    step 4: 'OrderDetailsProjection' - read model

--- app/backend/OrderDetailsProjection.scala
+++ app/backend/OrderDetailsProjection.scala
@@ -2,25 +2,47 @@ package backend
 
 import io.funcqrs.projections.Projection
 import model.read._
-import model.write.{OrderEvent, _}
+import model.write.{ OrderEvent, _ }
 import util.Lists
-
-import scala.concurrent.Future
 
 class OrderDetailsProjection(orderDetailsRepo: OrderDetailsRepo) extends Projection[OrderEvent] {
 
   def handleEvent = sync.HandleEvent {
-    case _ => ()
+    case evt: OrderWasCreated   => created(evt)
+    case evt: ItemWasAdded      => itemAdded(evt)
+    case evt: ItemWasRemoved    => itemRemoved(evt)
+    case evt: OrderWasCancelled => cancelled(evt)
+    case evt: OrderWasPayed     => payed(evt)
   }
 
-  def created(evt: OrderWasCreated): Unit = {}
+  def created(evt: OrderWasCreated): Unit =
+    orderDetailsRepo.save(OrderDetails(evt.number, Initiated))
 
-  def itemAdded(evt: ItemWasAdded): Unit = {}
+  def itemAdded(evt: ItemWasAdded): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      val updated = ItemDetails(evt.itemId, evt.name, evt.price) :: ord.items
+      ord.copy(items = updated, status = Open)
+    }
 
-  def itemRemoved(evt: ItemWasRemoved): Unit = {}
+  def itemRemoved(evt: ItemWasRemoved): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      val updatedItems = Lists.removeFirst(ord.items)(_.itemId == evt.itemId)
 
-  def cancelled(evt: OrderWasCancelled): Unit = {}
+      val status =
+        if (updatedItems.isEmpty) Initiated
+        else Open
 
-  def payed(evt: OrderWasPayed): Unit = {}
+      ord.copy(items = updatedItems, status = status)
+    }
+
+  def cancelled(evt: OrderWasCancelled): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      ord.copy(status = Cancelled)
+    }
+
+  def payed(evt: OrderWasPayed): Unit =
+    orderDetailsRepo.updateById(evt.number) { ord =>
+      ord.copy(status = Payed)
+    }
 
 }
